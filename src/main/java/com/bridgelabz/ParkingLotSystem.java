b34d6616c8fa2f1478b22746628314b40d65f8e1
Sudip Panja <panja370@gmail.com>
2021-11-09 14:59:47 +0530

    Added : welcome message

---
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To simulate parking lot system
+ *
+ * @author SUDIP PANJA
+ * @version '1.0-SNAPSHOT'
+ * @since 2021-11-09
+ */
+public class ParkingLotSystem {
+    /**
+     * Purpose : This method is created to print welcome message
+     */
+    public void printWelcomeMessage() {
+        System.out.println("Welcome To The Parking Lot System...");
+    }
+}
