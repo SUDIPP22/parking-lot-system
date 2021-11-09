e1a5306ca3e3a7a7890dda2d6a3cb6a48c24bf16
Sudip Panja <panja370@gmail.com>
2021-11-09 22:15:41 +0530

    Added : Test case and code to park the vehicle in the parking lot

--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -8,10 +8,33 @@ package com.bridgelabz;
  * @since 2021-11-09
  */
 public class ParkingLotSystem {
+    private Vehicle vehicle;
+
     /**
      * Purpose : This method is created to print welcome message
      */
     public void printWelcomeMessage() {
         System.out.println("Welcome To The Parking Lot System...");
+    }
+
+    /**
+     * Purpose : This method is created to park the vehicle
+     *
+     * @param vehicle : takes vehicle as parameter
+     */
+    public void park(Vehicle vehicle) {
+        if (this.vehicle == null)
+            this.vehicle = vehicle;
+    }
+
+    /**
+     * Purpose : This method is created to check
+     * the vehicle is parked or not
+     *
+     * @param vehicle : takes vehicle as parameter
+     * @return the vehicle is parked
+     */
+    public boolean isVehicleParked(Vehicle vehicle) {
+        return this.vehicle.equals(vehicle);
     }
 }
--- src/test/java/com/bridgelabz/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/ParkingLotSystemTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotSystemTest {
@@ -7,5 +8,14 @@ public class ParkingLotSystemTest {
     void givenMessage_ShouldPrintWelcomeMessage() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         parkingLotSystem.printWelcomeMessage();
+    }
+
+    @Test
+    void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        Vehicle vehicle = new Vehicle("PORSCHE", "WB-10KL2356");
+        parkingLotSystem.park(vehicle);
+        boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
+        Assertions.assertTrue(isParked);
     }
 }
