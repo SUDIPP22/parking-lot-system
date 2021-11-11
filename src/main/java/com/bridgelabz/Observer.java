35b5daea873f6323f40467508b555c3565fa1c78
Sudip Panja <panja370@gmail.com>
2021-11-12 00:51:02 +0530

    Added : Observer class for segregating owner and airport security entities

---
+++ src/main/java/com/bridgelabz/Observer.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To segregate the setParkingLotCapacity() method for parking lot system
+ *
+ * @author SUDIP PANJA
+ * @version '1.0-SNAPSHOT'
+ * @since 2021-11-11
+ */
+public interface Observer {
+    void setParkingLotCapacity();
+}
