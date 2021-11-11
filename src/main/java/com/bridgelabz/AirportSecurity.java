375a3e3457d2b9b16c9ba606998a708d20dc7c7a
Sudip Panja <panja370@gmail.com>
2021-11-12 00:49:50 +0530

    Added : AirportSecurity class to operate as an observer by airport security staff

---
+++ src/main/java/com/bridgelabz/AirportSecurity.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To operate as an observer by airport security staff
+ *
+ * @author SUDIP PANJA
+ * @version '1.0-SNAPSHOT'
+ * @since 2021-11-11
+ */
+public class AirportSecurity implements Observer {
+    private boolean isParkingLotFull;
+
+    /**
+     * Purpose : This method is created to set the status of full capacity of parking lot
+     */
+    @Override
+    public void setParkingLotCapacity() {
+        isParkingLotFull = true;
+    }
+
+    /**
+     * Purpose : This method is created to get back the status of full capacity of parking lot
+     *
+     * @return the status of the parking lot
+     */
+    public boolean getParkingLotStatusIfCapacityFull() {
+        return this.isParkingLotFull;
+    }
+}
