8ad4de1b6bec6a6f4dbe1c985062fa3a7d6f8ace
Sudip Panja <panja370@gmail.com>
2021-11-14 04:34:35 +0530

    Added : parking time entity for vehicle

--- src/main/java/com/bridgelabz/Vehicle.java
+++ src/main/java/com/bridgelabz/Vehicle.java
@@ -10,10 +10,12 @@ package com.bridgelabz;
 public class Vehicle {
     private final String name;
     private final String vehicleNumber;
+    private final String parkingTime;
 
-    public Vehicle(String name, String vehicleNumber) {
+    public Vehicle(String name, String vehicleNumber, String parkingTime) {
         this.name = name;
         this.vehicleNumber = vehicleNumber;
+        this.parkingTime = parkingTime;
     }
 
     public String getName() {
@@ -22,5 +24,9 @@ public class Vehicle {
 
     public String getVehicleNumber() {
         return vehicleNumber;
+    }
+
+    public String getParkingTime() {
+        return parkingTime;
     }
 }
